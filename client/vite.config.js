d4c870d0e61acd3134928535c3a229b6881b5e66
kmanish9301 <manishkharbade@gmail.com>
2026-02-24 21:56:23 +0530

    vite config changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-})
+export default ({ mode }) => {
+  // Load environment variables
+  // eslint-disable-next-line no-undef
+  const env = loadEnv(mode, process.cwd());
+
+  const port = env.PORT ? parseInt(env.PORT, 10) : 5174;
+
+  return defineConfig({
+    test: {
+      global: true,
+      environment: "jsdom",
+    },
+    plugins: [react(), tailwindcss()],
+    server: {
+      host: "0.0.0.0", // Expose on network for deployment
+      port,
+      allowedHosts: true,
+    },
+    preview: {
+      allowedHosts: true,
+    },
+    build: {
+      target: "esnext",
+      chunkSizeWarningLimit: 500,
+      outDir: "dist",
+      emptyOutDir: true,
+    },
+  });
+};
+
